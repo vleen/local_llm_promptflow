3fe32679fa1f24fdc8e551bf2c2b9e08d046fca5
vleen <37312117+vleen@users.noreply.github.com>
2024-03-20 22:27:31 +0200

    refactor(local_codellama_flow): minor flow tweaks (rename prompt file, remove some default input values in dag, remove flow-specific requirements file)

--- local_codellama_flow/components/call_local_llama.py
+++ local_codellama_flow/components/call_local_llama.py
@@ -3,6 +3,6 @@ from promptflow import tool
 
 
 @tool
-def call_local_llama(prompt: str, llm: Llama, max_tokens: int = 1024) -> str:
+def call_local_llama(prompt: str, llm: Llama, max_tokens: int = 2048) -> str:
     llm_response = llm(prompt=prompt, max_tokens=max_tokens, echo=False)
     return llm_response
