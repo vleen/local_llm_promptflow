a8ef580a8c794fd01a256e98d42f900cc58f867f
vleen <37312117+vleen@users.noreply.github.com>
2024-03-21 21:40:45 +0200

    refactor(call_local_llm_server.py): raise exceptions for http request and json decoding

--- local_server_codellama_flow/components/call_local_llm_server.py
+++ local_server_codellama_flow/components/call_local_llm_server.py
@@ -1,15 +1,22 @@
 import json
-
 import requests
-from llama_cpp import Llama
 from promptflow import tool
 
 
 @tool
 def call_local_llama(user_input: str, llm_endpoint: str, max_tokens: int = 2048) -> str:
-    # llm_response = llm(prompt=prompt, max_tokens=max_tokens, echo=False)
-    llm_response = requests.post(
-        llm_endpoint, data=json.dumps({"text": user_input, "max_tokens": max_tokens})
-    ).json()
 
-    return llm_response
+    try:
+        llm_response = requests.post(
+            llm_endpoint,
+            data=json.dumps({"text": user_input, "max_tokens": max_tokens}),
+        )
+        llm_response.raise_for_status()  # Raise an exception for non-2xx status codes
+        llm_response_json = llm_response.json()
+        return llm_response_json
+    except requests.RequestException as e:
+        # Handle request-related exceptions
+        raise ValueError(f"Error occurred during LLM request: {e}")
+    except json.JSONDecodeError as e:
+        # Handle JSON decoding error
+        raise ValueError(f"Error occurred during JSON decoding: {e}")
